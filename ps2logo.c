91723acf40f175d23ecea86b517e4ed9e6512a59
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-07-19 12:59:43 +0200

    Change license to MIT

--- ps2logo.c
+++ ps2logo.c
@@ -1,20 +1,9 @@
 /*
- * ps2logo.c - decrypt sector dump of PS2 logo
+ * Copyright (C) 2006-2013 Mathias Lafeldt <mathias.lafeldt@gmail.com>
  *
- * Copyright (C) 2006, 2009 misfire <misfire@xploderfreax.de>
+ * This file is part of ps2logo, the PS2 logo decryptor.
  *
- * ps2logo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ps2logo is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ps2logo.  If not, see <http://www.gnu.org/licenses/>.
+ * ps2logo is licensed under the terms of the MIT License. See LICENSE file.
  */
 
 #include <stdio.h>
