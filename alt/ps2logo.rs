c9e7cc6c1400a63ab72dd2586d1de41af524aba1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2017-04-23 10:59:12 +0200

    Add Rust implementation

---
+++ alt/ps2logo.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::stderr;
+use std::path::Path;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 2 {
+        writeln!(stderr(), "usage: ps2logo <input file> <output file>").unwrap();
+        process::exit(1);
+    }
+
+    let mut logo = vec![0xca; LOGO_SIZE].into_boxed_slice();
+
+    if let Err(err) = read_logo(Path::new(&args[0]), &mut logo) {
+        writeln!(stderr(), "error: failed to read logo: {}", err).unwrap();
+        process::exit(1);
+    }
+
+    decrypt_logo(&mut logo);
+
+    if let Err(err) = write_logo(Path::new(&args[1]), &logo) {
+        writeln!(stderr(), "error: failed to write logo: {}", err).unwrap();
+        process::exit(1);
+    }
+}
+
+fn read_logo(path: &Path, buf: &mut [u8]) -> Result<(), io::Error> {
+    File::open(path)?.read_exact(buf)
+}
+
+fn write_logo(path: &Path, buf: &[u8]) -> Result<(), io::Error> {
+    File::create(path)?.write_all(buf)
+}
+
+fn decrypt_logo(logo: &mut [u8]) {
+    let key = logo[0];
+    for x in logo {
+        *x ^= key;
+        *x = (*x << 3) | (*x >> 5);
+    }
+}
+
+// Size of PS2 logo in bytes: 12 sectors x 2048 bytes
+const LOGO_SIZE: usize = 12 * 2048;
